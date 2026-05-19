1b75d2ba76fffb76c94b238bb42216fcce349750
namdnp27 <phuongnam270299@gmail.com>
2026-05-19 08:07:31 -0400

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// error msg
+// let errorMessage = document.getElementById("error");
+// function errorFunc() {
+//   errorMessage.textContent = "Something went wrong. Please try agian!";
+// }
+
+function submitFunc() {
+  let userName = document.getElementById("name").value;
+  window.alert(`Hello ${userName}! Your message has been succesfully submitted`);
+}
